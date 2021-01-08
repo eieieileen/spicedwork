882b5817ab5ba3a96972fb3ff4a797fd4c76dab2
Eileen Lassche <eileenlassche@gmail.com>
2021-01-08 16:54:56 +0100

    finished Loop

---
+++ week_01/logType.js
@@ -0,0 +1,26 @@
+// function logType(arg) {
+//     if (typeof (arg) == Undefined) { 
+//         console.log("undefined!");
+//     } else if (typeof (arg) === null) {
+//         console.log("null");
+//     } else if (typeof (arg) == Number) {
+//         console.log("number");
+//     }
+// }
+
+// logType(23);
+
+
+// Write a function named logTypeX that expects a single argumentX and logs a different string depending on the type/value of the argument that is passed to it. The string it logs should be one of the following:
+
+// "undefined!"
+// "null!"
+// "number!"
+// "not a number!"
+// "string!"
+// "boolean!"
+// "bigint!"
+// "function!"
+// "object!"
+// "array!"
+// "I have no idea!
