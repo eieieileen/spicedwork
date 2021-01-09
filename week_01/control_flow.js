671fc7e8b6a191030aa90af5055c852de311968c
Eileen Lassche <eileenlassche@gmail.com>
2021-01-09 16:53:55 +0100

    header website

--- week_01/control_flow.js
+++ week_01/control_flow.js
@@ -29,3 +29,4 @@ for (var city in a) {
 // Then create a new empty object bXX and use a for..in loop to iterate over all of a's properties.
 // Give b properties whose names are the values from a and whose values are the property names from a.
 // The result should be an object that looks like this:
+
