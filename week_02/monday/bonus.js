583e678cec19dc489201ab46de875fd022504788
Eileen Lassche <eileenlassche@gmail.com>
2021-01-12 09:44:56 +0100

    fixed error

--- week_02/monday/bonus.js
+++ week_02/monday/bonus.js
@@ -1,4 +1,27 @@
+var totaler = function(getTotaler) {
+    return function(num) {
+        return num++;
+    };
+
+};
+
+var totaler = getTotaler();
 // Write a function that returns a function that can be called repeatedly and passed a number each time.
 // Each time it is called it should return the sum of the number that is passed in
 // and all other numbers that were passed in previous calls. That is,
 // it should return the sum of all the numbers that were ever passed to it.
+
+// var getCounter = function(num) {
+//     return function() {
+//         return num++;
+//     };
+// };
+//  var totaler = getTotaler();
+//  totaler(1); //1
+//  totaler(2); //3
+//  totaler(5); //8
+
+// var counter = getCounter(3);
+// counter(); //3
+// counter(); //4
+// counter(); //5
--- week_02/monday/error.js
+++ week_02/monday/error.js
@@ -1,28 +1,11 @@
 function number(num) {
-    if (num <= 0 || NaN) {
+    if (num <= 0 || isNaN(num)) {
         return "ERROR";
     } else if (num >= 1000000) {
-        return num;
-    } else if (num === 1000000) {
         return num;
     } else {
         number(num * 10);
     }
 }
 
-number(1888);
-
-// function countdown(num) {
-//     console.log(num);
-//     if (num === 0) {
-//         return; // this will end the function
-//     } else {
-//         countdown(num - 1);
-//     }
-// }
-
-// Write a function that expects a number as an argument. XX
-// If the value that is passed in is less than 0, equal to 0, or not a number, the function should return the string 'ERROR'. XX
-// If the number that is passed in is greater than or equal to 1000000 it should simply return the number. XX
-// Otherwise it should multiply the number by 10 however many times it takes to get a number that is greater than or equal
-// to 1000000 and;
+number(NaN);
