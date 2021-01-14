0a28f5a74b9638f35fb802d99796a59547cdf539
Eileen Lassche <eileenlassche@gmail.com>
2021-01-14 17:33:13 +0100

    finished opdr

---
+++ week_02/thursday/ex1/script.js
@@ -0,0 +1,9 @@
+console.log("sanity check");
+
+
+
+// Write a function that expects a string 
+// representing a selector to be passed as a parameter. XX
+// The function should find all the elements in the document 
+// that match the selector and change their style so that the 
+// text they contain is italic, underlined, and bold.
