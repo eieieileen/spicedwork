70db6684ab205f331e9ff2d4f621e082435443aa
Eileen Lassche <eileenlassche@gmail.com>
2021-01-15 17:46:30 +0100

    Ticker done

--- week_02/ticker/script.js
+++ week_02/ticker/script.js
@@ -1,22 +1,34 @@
-(function() {
+(function () {
     var headlines = document.getElementById("headlines");
+    var links = document.getElementsByTagName("a");
+    var initialPosition = headlines.offsetLeft; //1189
+    var requestId;
 
-    var initialPos = headlines.offsetLeft;
-    console.log(initialPos);
+    console.log("links:", links);
 
-    
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("mouseenter", function (event) {
+            console.log("event.target:", event.target);
+            cancelAnimationFrame(requestId);
+        });
 
+        links[i].addEventListener("mouseleave", function (event) {
+            console.log("event.target mouse leave:", event.target);
+            requestAnimationFrame(moveHeadlines);
+        });
+    }
     function moveHeadlines() {
-        initialPos--;
-        console.log(initialPos);
+        initialPosition--;
 
-        headlines.style.left = initialPos + "px";
+        if (initialPosition <= -links[0].offsetWidth) {
+            initialPosition += links[0].offsetWidth;
+            headlines.appendChild(links[0]);
+        }
 
-        requestAnimationFrame(moveHeadlines);
+        headlines.style.left = initialPosition + "px";
+        links[0].offsetWidth;
+        requestId = requestAnimationFrame(moveHeadlines);
+        // console.log(initialPosition);
     }
     moveHeadlines();
-
-    //get width first link tag(offsetwidth), get position off screen left, append it back to the end
-
 })(); // eileens iifi
-
