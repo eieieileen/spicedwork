637ede3cf8a3564dd4a5a630dfc17e653efb96e7
Eileen Lassche <eileenlassche@gmail.com>
2021-02-03 15:35:05 +0100

    finished part uno

---
+++ week_05/wednesday/fun-with-fs/index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+logSizes(__dirname + "/files");
+
+function logSizes(path) {
+    fs.readdir(path, { withFileTypes: true }, function (err, items) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        //console.log(items);
+        items.forEach(
+            (item) => {
+                if (item.isFile()) {
+                    //  console.log("item is a file:", item);
+                    fs.stat(path + "/" + item.name, function (err, stats) {
+                        if (err) {
+                            console.log("oopsie", err);
+                        } else {
+                            console.log(
+                                path + "/" + item.name + ":" + stats.size
+                            );
+                        }
+                    });
+                } else if (item.isDirectory()) {
+                    //console.log("item is a directory:", item);
+                    logSizes(path + "/" + item.name);
+                }
+            } //console.log(item.name, item.isDirectory(), item.isFile()) // looping through the array
+        );
+    });
+}
