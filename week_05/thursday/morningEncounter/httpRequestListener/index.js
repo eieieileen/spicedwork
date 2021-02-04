085e918ebef41a804c7055573b8e42ee4c2f7312
Eileen Lassche <eileenlassche@gmail.com>
2021-02-04 17:49:47 +0100

    not working yet

--- week_05/thursday/morningEncounter/httpRequestListener/index.js
+++ week_05/thursday/morningEncounter/httpRequestListener/index.js
@@ -8,10 +8,43 @@ const server = http.createServer((request, response) => {
     const { method, url } = request;
     const { headers } = request;
     const userAgent = headers["user-agent"]; //importent that headers are represented in lower-case only.
-    console.log(request.headers, request.method, request.url);
 
-    
+    console.log(request.headers, request.method, request.url); // for each request log the method, url, and request headers to the console
 
+    if (request.method == "GET" || request.method == "HEAD") {
+        response.setHeader("content-type", "text/html");
+        response.statusCode = 200;
+    }
+    if (request.method == "HEAD") {
+        response.end();
+    }
+    if (request.method == "GET") {
+        response.end(`<!doctype html>
+<html>
+<title>Hello World!</title>
+<p>Hello World!</p>
+</html>`);
+    }
+    if (request.method == "POST") {
+        let body = "";
+
+        request
+            .on("data", (chunk) => {
+                body += chunk;
+            })
+            .on("end", () => {
+                console.log(body);
+            });
+        response.setHeader("Location", "/");
+        response.statusCode = 302;
+    } 
+    if (request.method != "GET" && "HEAD" && "POST") {
+        response.statusCode = 405;
+    }
+
+    // response.setHeader("content-type", "text/html");
+    // response.write(body);
+    // response.end();
 });
 
 server.listen(8080, () => console.log(`Whaddup Ei im listening`));
