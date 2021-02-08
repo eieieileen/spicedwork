f56ebdcb558a9eecf13947760cf68351991a8a86
Eileen Lassche <eileenlassche@gmail.com>
2021-02-08 19:44:34 +0100

    finished exercises

--- week_06/monday/express-project/index.js
+++ week_06/monday/express-project/index.js
@@ -6,7 +6,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser()); // creates a key value pair obj
 app.use(express.static("./projects"));
 
-
 app.use((req, res, next) => {
     console.log(`MIDDLEWARE LOG : ${req.method} to ${req.url} route`);
     next();
@@ -19,11 +18,13 @@ app.use((req, res, next) => {
 // that they accept the cookie policy and a button to submit.
 // When users click on the button, a POST to /cookie should occur.
 
+//HOMEPAGE
 app.get("/cookie", (req, res) => {
-    //console.log("req.cookies 2nd", req.cookies);
-    // res.cookie("second cookie", "still exciting");
-    // res.cookie("authenticated", true);
-    res.send(`
+    if (!req.cookies.authenticated) {
+        //console.log("req.cookies 2nd", req.cookies);
+        // res.cookie("second cookie", "still exciting");
+        // res.cookie("authenticated", true);
+        res.send(`
     
     <h1>See a wonderful portfolio here</h1>
 
@@ -46,38 +47,82 @@ app.get("/cookie", (req, res) => {
     </form> 
     
     `);
+    } else {
+        res.redirect("/");
+    }
 });
 // <input type="submit" value="🍪 SUBMIT 🍪">
 
-// In the POST /cookie route,
-// check the value of the checkbox that was submitted.
-// If the cookie policy was accepted,
-// set a cookie to remember that fact across requests,
-// and redirect to the url the user had been attempting to navigate
-// to before being redirected to /cookie.
-// If the user did not accept the cookie policy,
-// send a page that tells the user they cannot use the site without accepting cookies.
-// You can set the cookie using the res.cookie method.
-
+//POST HOMEPAGE
 app.post("/cookie", (req, res) => {
     // console.log("a ${req.method} request was made to the ${req.url} route ");
     console.log("req.body:", req.body);
     const { checkbox } = req.body;
     if (checkbox) {
-
+        res.cookie("authenticated", "true");
         console.log("cookies accepted yaaay!");
+        res.redirect("/");
     } else {
         console.log("no cookies :(");
+        res.redirect("/cookieaccept");
     }
 });
 
-// app.get("/private", (req, res) => {
-//     console.log("req.cookies iuegfhiaush: ", req.cookies);
-//     if (req.cookies.authenticated) {
-//         res.send(
-//             `🍪<h1>In order to use this site you must accept the cookies 🍪</h1>`
-//         );
-//     }
-// });
+//REDIRECT PAGE
+app.get("/", (req, res) => {
+    if (req.cookies.authenticated) {
+        res.send(`
+        <h1>You have been redirected to the homepage</h1>
+    `);
+    } else {
+        res.redirect("/cookie");
+    }
+});
+
+//DID NOT ACCEPT COOKIES
+app.get("/cookieaccept", (req, res) => {
+    if (!req.cookies.authenticated) {
+        res.send(`
+     <h1>Unfortunately you have to accept the cookies :(</h1>
+
+    
+    
+    <h2>🍪I'm pretty sure you won't regret it!🍪</h2>
+
+    <p>To accept the 🍪🍪🍪 click the checkbox and submit 💥</p> 
+
+    <form method="POST">
+    
+    <input type="checkbox" name="checkbox">
+    
+    <br>
+    <br>
+    
+    
+    <button>🍪 SUBMIT 🍪</button>
+
+    </form> 
+    
+    `);
+    } else {
+        res.redirect("/");
+    }
+});
+
+//DID NOT ACCEPT COOKIES POST
+app.post("/cookieaccept", (req, res) => {
+    // console.log("a ${req.method} request was made to the ${req.url} route ");
+    console.log("req.body:", req.body);
+    const { checkbox } = req.body;
+    if (checkbox) {
+        res.cookie("authenticated", "true");
+        console.log("cookies accepted yaaay!");
+        res.redirect("/");
+    } else {
+        console.log("no cookies :(");
+        res.redirect("/cookie");
+        console.log("a ${req.method} request was made to the ${req.url} route");
+    }
+});
 
 app.listen(8080, () => console.log("Listening to my boss Eileen 🦁"));
