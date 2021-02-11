ded634360f32a37110601c02ed461c91b7121afc
Eileen Lassche <eileenlassche@gmail.com>
2021-02-11 14:17:00 +0100

    coding from class

---
+++ week_06/thursday/twitter-ticker/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./tickerjQuery"));
+
+app.get("/data.json", (req, res) => {
+    console.log("request for json has been made");
+
+    //1. get the token to be able to get tweets from the twitter API, when we have the token...
+    getToken((err, bearerToken) => {
+        console.log("inside the callback of getToken in index.js");
+        if (err) {
+            console.log("ooooohh no, something went wrong in getToken", err);
+            res.sendStatus(500);
+        } else {
+            console.log("We have the TOKEN YAY ME", bearerToken);
+        }
+    });
+    //2. make a request for tweets using the token.
+    getTweets(bearerToken, (err, tweets) => {
+        //3. filter the tweets we got in step 2.
+        const filteredTweets = filterTweets(tweets);
+    
+        //4. send back those filtered tweets as json to the client side.
+        res.json(filterTweets);
+    });
+});
+
+app.listen(8080, () => console.log("🌻 You look fabulous my queen ✨"));
---
+++ week_06/thursday/twitter-ticker/twitter.js
@@ -0,0 +1,52 @@
+const {twitterKey, twitterSecrets} = require("./secrets");
+const https = require("https");
+
+module.exports.getToken = function getToken(callbackToken) {
+    console.log("running getToken");
+
+    let credentials = `${twitterKey}:${twitterSecrets}`;
+    let encodedCredentials = Buffer.from(credentials).toString("base64");
+
+    const options = {
+        host: "api.twitter.com",
+        path: "/oauth2/token",
+        method: "POST",
+        headers: {
+            Authorization: `Basic ${encodedCredentials}`,
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+        }
+    };
+
+    function reqCallback(response) {
+        if (response.statusCode != 200) {
+            callbackToken(response.statusCode);
+            return;
+        }
+        let body = "";
+        response.on("data", (chunk) => {
+            body += chunk;
+        });
+        response.on("end", () => {
+            console.log("bodybodybody:", body);
+            let parsedBody = JSON.parse(body);
+            console.log("parsedBodybodybody", parsedBody.access_token);
+        });
+    }
+
+    const req = https.request(options, reqCallback);
+    req.end("grant_type=client:credentials");
+};
+
+module.exports.getTweets = function getTweets(bearerToken, callbackTweets) {
+    console.log("running getToken inside th callback of getToken in index.js");
+    //this function will get tweets from the twitter api
+    //this is also for me to complete 
+    const options = {
+        
+    }
+};
+
+module.exports.filterTweets = function filterTweets(tweets) {
+    //this function will clean up (filter) our tweet response from the twiiter api
+    //this is also for me to complete.
+};
