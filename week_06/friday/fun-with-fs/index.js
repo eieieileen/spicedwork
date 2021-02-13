ed5278a1e62ec9cc8fb98385245c870a662acfc7
Eileen Lassche <eileenlassche@gmail.com>
2021-02-13 17:10:49 +0100

    progress ex3

--- week_06/friday/fun-with-fs/index.js
+++ week_06/friday/fun-with-fs/index.js
@@ -1,32 +1,56 @@
-const fs = require("fs");
+// const fs = require("fs");
+let { readdir, stat } = require("fs");
+const { promisify } = require("util");
+
+readdir = promisify(readdir);
+stat = promisify(stat);
 
 function logSizes(path) {
-    fs.readdir(path, { withFileTypes: true }, function (err, items) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        //console.log(items);
-        items.forEach(
-            (item) => {
+    readdir(path, { withFileTypes: true })
+        .then((items) => {
+            items.forEach((item) => {
                 if (item.isFile()) {
-                    //  console.log("item is a file:", item);
-                    fs.stat(path + "/" + item.name, function (err, stats) {
-                        if (err) {
-                            console.log("oopsie", err);
-                        } else {
+                    stat(path + "/" + item.name)
+                        .then((stats) => {
                             console.log(
                                 path + "/" + item.name + ":" + stats.size
                             );
-                        }
-                    });
-                } else if (item.isDirectory()) {
+                        })
+                        .catch((err) => console.log("oopsie", err));
+                }else if (item.isDirectory()) {
                     //console.log("item is a directory:", item);
                     logSizes(path + "/" + item.name);
                 }
-            } //console.log(item.name, item.isDirectory(), item.isFile()) // looping through the array
-        );
-    });
+            });
+        })
+        .catch((err) => console.log(err));
+
+    // readdir(path, { withFileTypes: true }, function (err, items) {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+    //     //console.log(items);
+    //     items.forEach(
+    //         (item) => {
+    //             if (item.isFile()) {
+    //                 //  console.log("item is a file:", item);
+    //                 stat(path + "/" + item.name, function (err, stats) {
+    //                     if (err) {
+    //                         console.log("oopsie", err);
+    //                     } else {
+    //                         console.log(
+    //                             path + "/" + item.name + ":" + stats.size
+    //                         );
+    //                     }
+    //                 });
+    //             } else if (item.isDirectory()) {
+    //                 //console.log("item is a directory:", item);
+    //                 logSizes(path + "/" + item.name);
+    //             }
+    //         } //console.log(item.name, item.isDirectory(), item.isFile()) // looping through the array
+    //     );
+    // });
 }
 
 logSizes(__dirname + "/files");
