1d63de97f639231f9df167d3aff8c75ed6fa0841
Eileen Lassche <eileenlassche@gmail.com>
2021-02-15 21:53:13 +0100

    finished

--- week_07/monday/testing/exercises/2/albums.test.js
+++ week_07/monday/testing/exercises/2/albums.test.js
@@ -4,6 +4,19 @@ const spotify = require("./spotify");
 jest.mock("./spotify");
 
 test("album names are in alphabetical order", () => {
+    spotify.search.mockResolvedValue({
+        albums: {
+            items: [
+                {
+                    name: "Eileen",
+                },
+                {
+                    name: "Morrissey",
+                },
+            ],
+        },
+    });
+
     return getAlbumNames("meat loaf").then((albumNames) => {
         expect(albumNames).toEqual(albumNames.slice().sort());
     });
--- week_07/monday/testing/exercises/3/fn.js
+++ week_07/monday/testing/exercises/3/fn.js
@@ -1,12 +1,19 @@
 module.exports = function fn(str) {
-    if (typeof str != "string" && typeof str != []) {
+    let newMe = [];
+    if (typeof str != "string" && !Array.isArray(str)) {
         return null;
     } else if (typeof str == "string") {
         let newStr = str.split("").reverse().join("");
         return newStr;
     } else {
         for (let i = 0; i < str.length; i++) {
-            
+            if (typeof str[i] != "string" && !Array.isArray(str[i])) {
+                newMe.push(null);
+            } else {
+                let newStr = str[i].split("").reverse().join("");
+                newMe.push(newStr);
+            }
         }
+        return newMe;
     }
 };
