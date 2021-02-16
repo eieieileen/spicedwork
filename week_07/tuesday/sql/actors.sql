42b254f6348298577f91236d787ef032aa0d8f73
Eileen Lassche <eileenlassche@gmail.com>
2021-02-16 16:04:57 +0100

    finished thanks pete

--- week_07/tuesday/sql/actors.sql
+++ week_07/tuesday/sql/actors.sql
@@ -4,17 +4,17 @@ CREATE TABLE actors (
     id SERIAL primary key,
     name VARCHAR(255) NOT NULL,
     age INT,
-    "Number of Oscars" VARCHAR
+    number_of_oscars VARCHAR
 );
 
-INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('Leonardo DiCaprio', '41', 1);
-INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('Jennifer Lawrence', '25', 1);
-INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('Samuel L. Jackson', '67', 0);
-INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('Meryl Streep', '66', 3);
-INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('John Cho', '43', 0);
+INSERT INTO actors (name, age,  number_of_oscars) VALUES ('Leonardo DiCaprio', '41', 1);
+INSERT INTO actors (name, age,  number_of_oscars) VALUES ('Jennifer Lawrence', '25', 1);
+INSERT INTO actors (name, age,  number_of_oscars) VALUES ('Samuel L. Jackson', '67', 0);
+INSERT INTO actors (name, age,  number_of_oscars) VALUES ('Meryl Streep', '66', 3);
+INSERT INTO actors (name, age,  number_of_oscars) VALUES ('John Cho', '43', 0);
 
 -- Which actors have more than one oscar?
--- SELECT name FROM actors WHERE "Number of Oscars" > 1;
+SELECT name FROM actors WHERE number_of_oscars > '1';
 
 -- -- Which actors are older than 30 years old?
--- SELECT name FROM actors WHERE age > 30;
+SELECT name FROM actors WHERE age > '30';
