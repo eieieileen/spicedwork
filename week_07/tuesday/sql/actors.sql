b976f451ad0811be2bdb5d62acebc3175ebbff52
Eileen Lassche <eileenlassche@gmail.com>
2021-02-16 12:46:57 +0100

    its not working idk why

---
+++ week_07/tuesday/sql/actors.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS actors;
+
+CREATE TABLE actors (
+    id SERIAL primary key,
+    name VARCHAR(255) NOT NULL,
+    age INT,
+    "Number of Oscars" VARCHAR
+);
+
+INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('Leonardo DiCaprio', '41', 1);
+INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('Jennifer Lawrence', '25', 1);
+INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('Samuel L. Jackson', '67', 0);
+INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('Meryl Streep', '66', 3);
+INSERT INTO actors (name, age,  "Number of Oscars") VALUES ('John Cho', '43', 0);
+
+-- Which actors have more than one oscar?
+-- SELECT name FROM actors WHERE "Number of Oscars" > 1;
+
+-- -- Which actors are older than 30 years old?
+-- SELECT name FROM actors WHERE age > 30;
